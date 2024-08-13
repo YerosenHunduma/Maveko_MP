bab5c9d0a3702c16ccfd738ebcfb671c706ac65a
YerosenHunduma <yerosenhunduma@gmail.com>
2024-08-13 11:45:32 +0300

    configure database connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectToDb from './Config/dbConfig.js';
+
 dotenv.config();
 const PORT = process.env.PORT || 6000;
 
@@ -26,6 +28,12 @@ app.get('/', (req, res) => {
 
 // Start the server
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+connectToDb()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Database connection error:', error.message);
+    });
