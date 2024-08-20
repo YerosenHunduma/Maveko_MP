91648d036e2c8eb5f2340176287b91e8fa205b5a
YerosenHunduma <yerosenhunduma@gmail.com>
2024-08-20 20:16:44 +0300

    add limit function to the category fetch endpoint

--- controllers/category.controllers.js
+++ controllers/category.controllers.js
@@ -2,14 +2,19 @@ import categoryModel from '../models/category.model.js';
 import { errorHandler } from '../utils/errorHandler.js';
 
 export const newCategories = async (req, res, next) => {
-    const { category } = req.body;
+    const { category, description } = req.body;
     try {
         if (!category) {
             return next(new errorHandler('Missing required fields.', 400));
         }
 
+        if (await categoryModel.findOne({ category })) {
+            return next(new errorHandler('Category already exists.', 409));
+        }
+
         const newCategory = new categoryModel({
-            category
+            category,
+            description
         });
 
         await newCategory.save();
@@ -21,8 +26,13 @@ export const newCategories = async (req, res, next) => {
 };
 
 export const getAllCategories = async (req, res, next) => {
+    const { perPage } = req.query;
+
+    const limit = perPage == 'some' ? 5 : undefined;
+
     try {
-        const categories = await categoryModel.find().select('category');
+        const categories = await categoryModel.find().select('-__v').limit(limit);
+
         res.status(200).json(categories);
     } catch (error) {
         next(error);
