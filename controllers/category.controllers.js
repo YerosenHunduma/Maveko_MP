1ba68254a0c5e495e6a343e29c9375d8e3241e95
YerosenHunduma <yerosenhunduma@gmail.com>
2024-08-21 11:37:53 +0300

    create an endpoint to fetch product by category

--- controllers/category.controllers.js
+++ controllers/category.controllers.js
@@ -1,4 +1,5 @@
 import categoryModel from '../models/category.model.js';
+import productsModel from '../models/products.model.js';
 import { errorHandler } from '../utils/errorHandler.js';
 
 export const newCategories = async (req, res, next) => {
@@ -34,6 +35,19 @@ export const getAllCategories = async (req, res, next) => {
         const categories = await categoryModel.find().select('-__v').limit(limit);
 
         res.status(200).json(categories);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getProductByCategory = async (req, res, next) => {
+    const { category } = req.query;
+    try {
+        const products = await productsModel.find({ ancestry: { $regex: category, $options: 'i' } }).select('-__v');
+        if (!products.length) {
+            return next(new errorHandler('No products found in this category.', 404));
+        }
+        res.status(200).json(products);
     } catch (error) {
         next(error);
     }
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllCategories, newCategories } from '../controllers/category.controllers.js';
+import { getAllCategories, getProductByCategory, newCategories } from '../controllers/category.controllers.js';
 
 const router = express.Router();
 
 router.post('/new-category', newCategories);
 router.get('/get-all-categories', getAllCategories);
+router.get('/getCategory-product', getProductByCategory);
 
 export default router;
